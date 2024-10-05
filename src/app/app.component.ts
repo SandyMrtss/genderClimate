aef805ab983262d2e9795b6b592df1af93f03e72
Maya <maya.camps@estudiantat.upc.edu>
2024-10-05 20:22:31 +0200

    map pop-up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,8 +21,9 @@ export class AppComponent implements OnInit {
   }
 
   private initMap(): void {
-    this.map = L.map('map').setView([0.00, 0.00], 1.5);
+    this.map = L.map('map').setView([30, 0.00], 1.5);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom:8,
       attribution: '© OpenStreetMap contributors'
     }).addTo(this.map);
 
--- src/app/components/layer/layer.component.ts
+++ src/app/components/layer/layer.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import * as L from 'leaflet';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-layer',
@@ -14,13 +15,15 @@ import * as L from 'leaflet';
   
   private markersLayer!: L.LayerGroup;
 
-
   public addMarkers(map: L.Map): L.Map{
     this.markersLayer = L.layerGroup().addTo(map);
+    const popupContent = `<div style="max-width: 300px; max-height: 200px; overflow-y: auto;"> 
+    <h3> Bangladesh Floodings </h3> 
+    <img src="/assets/img/bangladesh.JPG" style="width: 100%; height: auto;"/> 
+    <p>In 1991, during cyclone disasters, 90% of the 140,000 people who died were women because they were not taught how to swim. </p> 
+    </div>`;
+    L.marker([23.68,90.35]).addTo(this.markersLayer).bindPopup(popupContent);
 
-    L.marker([0,0]).addTo(this.markersLayer)
-      .bindPopup('holi')
-      .openPopup();
     return map;
   }
 }
