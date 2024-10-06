39777924075aede5950e97f1bc06803bc1f5c18c
Sandy Martos <bebesubi2@gmail.com>
2024-10-06 12:28:19 +0200

    adds views and sedac map

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { MapComponent } from './components/map/map.component';
+import { LayerComponent } from './components/layer/layer.component';
+import { ViewHotspotsComponent } from './components/view-hotspots/view-hotspots.component';
+import { MainViewComponent } from './components/main-view/main-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'layers', component: ViewHotspotsComponent },
+    { path: 'home', component: MainViewComponent },
+    { path: '', component: MainViewComponent }
+
+
+];
