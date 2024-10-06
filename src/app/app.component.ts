0a76434118736ef1dcf6c2adff2a4bac7dcf8ea1
Sandy Martos <bebesubi2@gmail.com>
2024-10-06 10:20:03 +0200

    adds marker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,62 +13,22 @@ import { MapComponent } from "./components/map/map.component";
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-  private map!: MapComponent;
-  private markersLayer!: L.LayerGroup;
-  private circleLayer!: L.LayerGroup;
-  markersLayerActive: boolean = true;
-  circleLayerActive: boolean = true;
+  private map!: L.Map;
+  private layerComponent: LayerComponent = new LayerComponent();
+  
 
   ngOnInit() {
     this.initMap();
   }
 
   private initMap(): void {
-    this.map = new MapComponent();
-   /* this.map = L.map('map').setView([51.505, -0.09], 13);
+    this.map = L.map('map').setView([0.00, 0.00], 1.5);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '© OpenStreetMap contributors'
     }).addTo(this.map);
 
-    this.initLayers();*/
-  }
-/*
-  private initLayers(): void {
-    this.markersLayer = L.layerGroup().addTo(this.map);
-    this.circleLayer = L.layerGroup().addTo(this.map);
-
-    L.marker([51.5, -0.09]).addTo(this.markersLayer)
-      .bindPopup('A sample marker!')
-      .openPopup();
-
-    L.marker([51.51, -0.1]).addTo(this.markersLayer)
-      .bindPopup('Another marker!');
-
-    L.circle([51.508, -0.11], {
-      color: 'red',
-      fillColor: '#f03',
-      fillOpacity: 0.5,
-      radius: 500
-    }).addTo(this.circleLayer);
+    this.layerComponent.addMarkers(this.map);
   }
 
-  toggleMarkersLayer(active: boolean): void {
-    this.markersLayerActive = active;
-    if (active) {
-      this.map.addLayer(this.markersLayer);
-    } else {
-      this.map.removeLayer(this.markersLayer);
-    }
-  }
 
-  toggleCircleLayer(active: boolean): void {
-    this.circleLayerActive = active;
-    if (active) {
-      this.map.addLayer(this.circleLayer);
-    } else {
-      this.map.removeLayer(this.circleLayer);
-    }
-  }
-
-  */
 }
--- src/app/components/layer/layer.component.ts
+++ src/app/components/layer/layer.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import * as L from 'leaflet';
 
@@ -10,13 +11,31 @@ import * as L from 'leaflet';
   styleUrl: './layer.component.css'
 })
 
-  export class LayerComponent {
-  @Input() name!: string;
-  @Input() active: boolean = false;
-  @Output() layerToggle = new EventEmitter<boolean>();
+export class LayerComponent {
 
-  toggleLayer() {
-    this.active = !this.active;
-    this.layerToggle.emit(this.active);
+  private markersLayer!: L.LayerGroup;
+
+  public addMarkers(map: L.Map): L.Map {
+
+    this.markersLayer = L.layerGroup().addTo(map);
+
+    L.marker([23.68590, 90.3563]).addTo(this.markersLayer)
+      .bindPopup('Bangladesh');
+
+    L.marker([7.8731, 80.7718]).addTo(this.markersLayer)
+      .bindPopup('.');
+
+    L.marker([30.211, -89.162]).addTo(this.markersLayer)
+      .bindPopup('.');
+
+    L.marker([13.736, 100.523]).addTo(this.markersLayer)
+      .bindPopup('.');
+
+    L.marker([1.286, 36.8219]).addTo(this.markersLayer)
+      .bindPopup('.');
+
+    return map;
   }
+
+
 }
