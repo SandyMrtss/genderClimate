5b806b7d1059290d5efcaf5119c284c936f585ba
Maya <maya.camps@estudiantat.upc.edu>
2024-10-06 13:06:59 +0200

    solutionsView

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { MapComponent } from './components/map/map.component';
 import { LayerComponent } from './components/layer/layer.component';
 import { ViewHotspotsComponent } from './components/view-hotspots/view-hotspots.component';
 import { MainViewComponent } from './components/main-view/main-view.component';
+import { SolutionViewComponent } from './components/solution-view/solution-view.component';
 
 export const routes: Routes = [
     { path: 'layers', component: ViewHotspotsComponent },
+    { path: 'solutions', component: SolutionViewComponent },
     { path: 'home', component: MainViewComponent },
     { path: '', component: MainViewComponent }
 
